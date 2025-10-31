34d513b29ae03907e9821510282c63fb4b1236c0
Abdelilah Isswa <tomhary66@gmail.com>
2025-10-31 14:58:21 +0100

    js varibales

---
+++ script.js
@@ -0,0 +1,21 @@
+ let worldlist ='leorem sdfedfzezf'
+let typingspeed = document.querySelector("#typingspeed")
+
+let input = document.querySelector('#input');
+let world = document.querySelector("#word")
+let elementfolt = document.getElementById("folt");
+let timer = document.getElementById("timer");
+let index =0
+let typed =0
+let folt =0
+let plus =0
+let time =0
+let x =0;
+let started= false;
+let taux_de_précision= 0;
+let timerid;
+let score = document.getElementsByClassName("score")[0]
+let restart = document.querySelector(".restartbtn")
+ restart.style.display = 'none';
+ typingspeed.style.display ='none';
+ let yourscore=document.getElementsByClassName("yourscore")[0];
